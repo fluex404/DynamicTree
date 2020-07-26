75880ecf50674f72267538080f4075d70d9c1865
= <isaabqari@gmail.com>
2020-07-26 08:01:49 +0700

    first commit

---
+++ src/main/java/com/fluex404/DynamicTree/DynamicTreeApplication.java
@@ -0,0 +1,13 @@
+package com.fluex404.DynamicTree;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DynamicTreeApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DynamicTreeApplication.class, args);
+	}
+
+}
